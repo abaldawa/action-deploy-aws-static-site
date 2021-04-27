9684176fcb0054bd868026ec640e0e17240f8f1b
Abhijit <abhi.baldawa@gmail.com>
2021-04-27 18:11:59 +0200

    Added fix to handle 404 routing error

---
+++ src/static-page-stack.test.ts
@@ -0,0 +1,30 @@
+import { expect as expectCDK, haveResource } from "@aws-cdk/assert";
+import * as cdk from "@aws-cdk/core";
+import { StaticPageStack } from "./static-page-stack";
+
+test("AWS s3 static site template handling 404 routing error", () => {
+  const app = new cdk.App();
+  const stack = new StaticPageStack(app, "MyTestStack", {
+    fullDomain: "sub.example.com",
+    folder: "./images",
+    stackName: "MyTestStack",
+  });
+  expectCDK(stack).to(
+    haveResource("AWS::S3::Bucket", {
+      WebsiteConfiguration: {
+        IndexDocument: "index.html",
+        ErrorDocument: "error.html",
+        RoutingRules: [
+          {
+            RoutingRuleCondition: {
+              HttpErrorCodeReturnedEquals: "404",
+            },
+            RedirectRule: {
+              ReplaceKeyWith: "/index.html",
+            },
+          },
+        ],
+      },
+    })
+  );
+});
